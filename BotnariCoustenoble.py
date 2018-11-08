05d69b2325664837fea1578ba3fb7c3548aa11d0
jul <coustenoble.julie@gmail.com>
2018-11-08 17:54:55 +0100

    add the plan

--- BotnariCoustenoble.py
+++ BotnariCoustenoble.py
@@ -6,3 +6,10 @@ Created on Thu Nov  8 17:34:07 2018
 @author: joujoucous
 """
 
+#récuperer les deux csv sur internet
+#https://www.science-emergence.com/Articles/T%C3%A9l%C3%A9charger-un-fichier-pdf-du-web-avec-python/
+
+#faire la carte du monde avec les nationalitées des artistes
+    #transformet la nationalité en pays
+
+#faire l'histogramme avec les tailles des oeuvres ou ave le nombre d'oeuvres crées par années
