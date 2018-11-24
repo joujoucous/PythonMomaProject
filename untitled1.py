638c757dd9590e663b708b162a3a591e69c060e3
jul <coustenoble.julie@gmail.com>
2018-11-24 13:53:37 +0100

    map work

--- untitled1.py
+++ untitled1.py
@@ -1,8 +1,11 @@
 import folium
 import json
 
+#préparation des données géographiques
+#préparation des données numériques
 
-# build the map
+
+#création d’une instance de Folium.Map
 coords = (46.6299767,1.8489683)
 map = folium.Map(location=coords, tiles='OpenStreetMap', zoom_start=2)
 
@@ -21,5 +24,21 @@ fGeo = open('map.geojson', 'r', encoding='utf8')
 g = json.loads(fGeo.read())
 fGeo.close()
 geo_data["features"].extend((g["features"])) # add current geojson data to master dict
+
+#data prep
+
+
+#application la méthode choropleth() à l'instance map
+map.choropleth(
+    geo_data=geo_data,
+    name='choropleth',
+    data=df,
+    columns=['Pays d origine', 'Nombre total d artiste'], # data key/value pair
+    key_on='feature.properties.code', # corresponding layer in GeoJSON
+    fill_color='YlGn',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='Origine des artistes du MOMA'
+)
 
 map.save(outfile='map.html')
